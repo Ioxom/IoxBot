2a6296ac5cc9809d75a536988f568392b9160e90
Ioxom <66223394+Ioxom@users.noreply.github.com>
2020-06-21 11:29:15 -0500

    we have now surpassed thonky bot in every way imaginable

--- index.js
+++ index.js
@@ -8,49 +8,50 @@ client.login ('NzIyODM1MjkwNjQ0ODA3NzEx.Xuo3vQ.ZFGQOKOxVdE0VosQMcDjHc4d8Ww') ;
 
 //sends a message in console when the bot successfully starts up
 client.once ('ready', () =>{
-    console.log ('bot initialized successfully') ;
+	console.log ('bot initialized successfully') ;
 })
 
 //this is where new commands are added
 client.on ('message', msg =>{
-    var args = msg.content.substring (prefix.length) .split (" ");
-    switch (args[0]) {
-        case 'belt':
-            msg.channel.send ('ALRIGHT YOU AIN\'T LISTENIN\' SO YOU\'RE GETTING THE BELT!') ;
-            msg.channel.send ('https://cdn.discordapp.com/attachments/618926084750180363/721009676849905725/belt.jpg')
-            break;
-        case 'uno_reverse_card':
-            msg.channel.send ('https://cdn.discordapp.com/attachments/642003331656581170/717832539896807434/nxeiZ78_ZCkQvMGkZ7fSTmixJ2ey4JCSlOuyKQL6Gos-1.png')
-            break;
-        case 'are_they_groovin?':
-            //set the variable "groovin" to a random number between one and ten
-            var groovin = (Math.floor (Math.random() * 10 ))
-            //show the result of the random number generation for testing purposes
-            console.log (groovin)
-            if (groovin = 1) {
-                msg.channel.send ('https://cdn.discordapp.com/attachments/618926084750180363/717833399825334303/Screen_Shot_2019-07-31_at_12.png')
-                return;
-            }
-            if (groovin = 1,2,3,4,5,6,7,8,9,10) {
-                msg.channel.send ('https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQnpJBB55my5LQ-7rf3LVTqMdAYwVPJ2VwnD8dbgq22qEA4DiiC&usqp=CAU')
-                return;
-            }
-        case 'BotInfo':
-            switch (args[1]) {
-                case 'version':
-                    msg.channel.send ('```IoxBot version is ' + version + '```');
-                    break;
+	var args = msg.content.substring (prefix.length) .split (" ");
+	switch (args[0]) {
+		case 'belt':
+			msg.channel.send ('ALRIGHT YOU AIN\'T LISTENIN\' SO YOU\'RE GETTING THE BELT!') ;
+			msg.channel.send ('https://cdn.discordapp.com/attachments/618926084750180363/721009676849905725/belt.jpg')
+			break;
+		case 'uno_reverse_card':
+			msg.channel.send ('https://cdn.discordapp.com/attachments/642003331656581170/717832539896807434/nxeiZ78_ZCkQvMGkZ7fSTmixJ2ey4JCSlOuyKQL6Gos-1.png')
+			break;
+		case 'are_they_groovin?':
+			var groovin = (Math.floor (Math.random() * 10 ))
+			console.log (groovin)
+			if (groovin = 1) {
+				msg.channel.send ('https://cdn.discordapp.com/attachments/618926084750180363/717833399825334303/Screen_Shot_2019-07-31_at_12.png')
+				return;
+			}
+			if (groovin = 1,2,3,4,5,6,7,8,9,10) {
+				msg.channel.send ('https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQnpJBB55my5LQ-7rf3LVTqMdAYwVPJ2VwnD8dbgq22qEA4DiiC&usqp=CAU')
+				return;
+			}
+		case 'BotInfo':
+			switch (args[1]) {
+				case 'version':
+					msg.channel.send ('```IoxBot version is ' + version + '```');
+					break;
 
-                case 'author':
-                    msg.channel.send ('```IoxBot, made by Ioxom```');
-                    break;
-                case 'commands':
-                    msg.channel.send ('```Commands are ' + commands + '```')
-                    break;
-                case 'GitHub':
-                    msg.channel.send ('```https://github.com/Ioxom/IoxBot```')
-                    break;
+				case 'author':
+					msg.channel.send ('```IoxBot, made by Ioxom```');
+					break;
+				case 'commands':
+					msg.channel.send ('```Commands are ' + commands + '```')
+					break;
+				case 'GitHub':
+					msg.channel.send ('```https://github.com/Ioxom/IoxBot```')
+					break;
 
-            }
-    }
+			}
+		case 'what\'s_a_tortoise':
+			msg.channel.send ('As we all know, an underdeveloped sad excuse for a turtle.')
+			break;
+	}
 })
