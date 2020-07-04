8a700d3a207646408d36515d7e9ad2e7d43facd9
Ioxom <jackwheppner@gmail.com>
2020-07-03 19:55:55 -0500

    begin the reformat revolution

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, MessageAttachment } = require('discord.js');
+const client = new Client();
 const prefix = ('-');
 const token = ('NzIyODM1MjkwNjQ0ODA3NzEx.Xuo3vQ.ZFGQOKOxVdE0VosQMcDjHc4d8Ww');
 const version = '1.0.0';
@@ -11,14 +11,19 @@ client.once ('ready', () =>{
 	console.log ('bot initialized successfully') ;
 })
 
+client.on ('message', msg => {
+	if (msg.content === '-belt') {
+	  // Create the belt attachment using MessageAttachment
+	  const beltAttachment = new MessageAttachment('https://cdn.discordapp.com/attachments/618926084750180363/721009676849905725/belt.jpg');
+	  msg.channel.send ('ALRIGHT YOU AIN\'T LISTENIN\' SO YOU\'RE GETTING THE BELT!')
+	  msg.channel.send (beltAttachment);
+	}
+});
+
 //this is where new commands are added
 client.on ('message', msg =>{
 	var args = msg.content.substring (prefix.length) .split (" ");
 	switch (args[0]) {
-		case 'belt':
-			msg.channel.send ('ALRIGHT YOU AIN\'T LISTENIN\' SO YOU\'RE GETTING THE BELT!') ;
-			msg.channel.send ('https://cdn.discordapp.com/attachments/618926084750180363/721009676849905725/belt.jpg')
-			break;
 		case 'uno_reverse_card':
 			msg.channel.send ('https://cdn.discordapp.com/attachments/642003331656581170/717832539896807434/nxeiZ78_ZCkQvMGkZ7fSTmixJ2ey4JCSlOuyKQL6Gos-1.png')
 			break;
@@ -33,25 +38,27 @@ client.on ('message', msg =>{
 				msg.channel.send ('https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQnpJBB55my5LQ-7rf3LVTqMdAYwVPJ2VwnD8dbgq22qEA4DiiC&usqp=CAU')
 				return;
 			}
+		case 'what\'s_a_tortoise':
+			msg.channel.send ('As we all know, an underdeveloped sad excuse for a turtle.')
+			break;
 		case 'BotInfo':
 			switch (args[1]) {
 				case 'version':
 					msg.channel.send ('```IoxBot version is ' + version + '```');
-					break;
-
+					return;
 				case 'author':
 					msg.channel.send ('```IoxBot, made by Ioxom```');
-					break;
+					return;
 				case 'commands':
 					msg.channel.send ('```Commands are ' + commands + '```')
-					break;
+					return;
 				case 'GitHub':
 					msg.channel.send ('```https://github.com/Ioxom/IoxBot```')
-					break;
+					return;
 
 			}
-		case 'what\'s_a_tortoise':
-			msg.channel.send ('As we all know, an underdeveloped sad excuse for a turtle.')
-			break;
+
 	}
 })
+
+a
