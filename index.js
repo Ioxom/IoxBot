59707c063f642ad94a3386c2fa1315a45df4107f
Ioxom <jackwheppner@gmail.com>
2020-08-30 15:30:38 -0500

    Initial commit - v1.0

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const config = require('./config.json');
+//requires
+const config = require ('./configuration/config.json');
+if (config.lang === 'en_ca') var lang = require ('./configuration/lang/en_ca.json');
 const { Client, MessageAttachment, MessageEmbed } = require ('discord.js');
+const updates = require('./configuration/update.json');
 const client = new Client ();
 //we're storing variables in the config now, makes this file very clean
 //logs in the bot using the token from the config file
@@ -23,8 +26,8 @@ client.on ('message', msg => {
 
 	//function for generating random numbers within parameters
 	//creates the function with parameters for the maximum value and the minimum value
+	//generates a random number between the minimum and maximum defined earlier
 	function getrndInteger (min, max) {
-		//generates a random number between the parameters using the power of math
 		return (Math.floor (Math.random () * (max - min) ) + min);
 	}
 
@@ -32,8 +35,8 @@ client.on ('message', msg => {
 	//creates the function with a parameter for what command was used
 	function logUsedCommand (commandUsed) {
 		//checks if logCommandUses in the config is true
+		//prints the id of the user and then "used [command]"
 		if (config.logCommandUses === 'true') {
-			//prints the id of the user and then "used [command]"
 			console.log ((msg.author.tag) + ' used ' + (commandUsed));
 		}
 	}
@@ -44,17 +47,14 @@ client.on ('message', msg => {
 		//generates two random numbers between 0 and 75
 		var integer1 = (getrndInteger (0, 75));
 		var integer2 = (getrndInteger (0, 75));
-		//adds a zero to the end of the number if it's below 10, preventing the result from being the wrong colour
+		//adds a zero to the end of integer1/integer2 if either are below 10, preventing the result from being the wrong colour
 		if (integer1 < 10) {
 			integer1 = (integer1) + ('0');
-		}
-		//does the same for integer2
-		if (integer2 < 10) {
+		} else if (integer2 < 10) {
 			integer2 = (integer2) + ('0');
 		}
 		//adds the values to the selected colour to create a colour hex code
 		shadeResult = ((integer1) + (colour) + (integer2))
-		//sends the result to the line where the funtion was called
 		return shadeResult;
 	}
 
@@ -64,8 +64,11 @@ client.on ('message', msg => {
 		const numbers = '0123456789';
 		//add all the characters together, giving numbers a 2/3 chance to be the result and letters 1/3
 		var characters = (letters + numbers + numbers + numbers + numbers + numbers + numbers);
+		function selectchars() {
+			return (characters[(Math.ceil (Math.random () * 87))]);
+		}
 		//first character can only be a number to prevent invalid colours
-		var result = (numbers[(Math.ceil (Math.random () * 10))] + characters[(Math.ceil (Math.random () * 87))] + characters[(Math.ceil (Math.random () * 87))] + characters[(Math.ceil (Math.random () * 87))] + characters[(Math.ceil (Math.random () * 87))] + characters[(Math.ceil (Math.random () * 87))]);
+		var result = (numbers[(Math.ceil (Math.random () * 10))] + selectchars() + selectchars() + selectchars() + selectchars() + selectchars());
 		return result;
 	}
 
@@ -74,10 +77,8 @@ client.on ('message', msg => {
 	if (config.enablePrefixlessCommands === 'true') {
 		var args = msg.content.substring ("".length) .split (" ");
 		switch (args[0]) {
-			//checks if the message starts with "IoxBot"
-			case 'IoxBot':
-			case 'Ioxbot':
-			case 'ioxbot':
+			//checks if the message starts with "IoxBot" in any capitalization, hooray for case spam
+			case 'IoxBot': case 'Ioxbot': case 'ioxbot': case 'ioxBot':
 				const whomstAttachment = new MessageAttachment ('https://cdn.discordapp.com/attachments/719955731821887602/733807045874286622/whomst.jpg');
 				if (msg.author.bot) return;
 				msg.channel.send (whomstAttachment);	
@@ -89,12 +90,9 @@ client.on ('message', msg => {
 				msg.channel.send (ripAttachment);
 				logUsedCommand ('rip');
 				break;
-			case 'hello':
-			case 'Hello':
+			case 'hello': case 'Hello':
 				switch (args[1]) {
-					case 'IoxBot':
-					case 'Ioxbot':
-					case 'ioxbot':
+					case 'IoxBot': case 'Ioxbot': case 'ioxbot': case 'ioxBot':
 						if (msg.author.bot) return;
 						//sends "hello" accompanied by the id of the author of the command message, plus angle brackets and an @ so it's a ping
 						msg.channel.send (('Hello ') + ('<@')  + (msg.author) + ('>'));
@@ -113,16 +111,12 @@ client.on ('message', msg => {
 	}
 
 	//normal commands
-	//checks if the author of the message is a bot or doesn't have the prefix, in both cases it cancels the command
-	if (msg.author.bot || !msg.content.startsWith (config.prefix)) return;
 	//splits the arguments at every space
 	var args = msg.content.substring ((config.prefix).length) .split (" ");
 	switch (args[0]) {
 
 		//experimental commands
-		case 'E':
-		case 'e':
-		case 'experimental':
+		case 'E': case 'e': case 'experimental':
 		if (config.enableExperimentalCommands === 'true') {
 			switch (args[1]) {
 				case 'colouredembed':
@@ -134,23 +128,29 @@ client.on ('message', msg => {
 					break;
 				case 'loop':
 					switch (args[2]) {
-						default:
-							//sets the number of times to loop to the third part of the sent message (must be a number)
-							var amountToLoop = (args[2]);
+					default:
+						//sets the number of times to loop to the third part of the sent message (must be a number)
+						var amountToLoop = (args[2]);
 						switch (args[3]) {
-							default:
-								//sets the message to send based on the final argument of the command
-								var messageToSend = (args[3]);
-								//repeats sending the message for the amount of times defined in amountToLoop
-								for (var loopCounter = 0; loopCounter < amountToLoop; loopCounter++) {
-									msg.channel.send (messageToSend);
-								}
-								logUsedCommand ('-experimental loop ' + '[repeats: ' + (amountToLoop) + ']' + ' [message: ' + (messageToSend) + ']');
-								break;
+						default:
+							//error messages
+							if (isNaN(args[2]) && Array.isArray(args[2])) {
+								msg.channel.send ('invalid third argument found. use -e help for the proper command usage');
+							} else if ((!args[2]) && (!args[3])) {
+								msg.channel.send ('missing third and fourth argument. use -e help for the proper command usage');
 							}
+							//sets the message to send based on the final argument of the command
+							var messageToSend = (args[3]);
+							//repeats sending the message for the amount of times defined in amountToLoop
+							for (var loopCounter = 0; loopCounter < amountToLoop; loopCounter++) {
+								msg.channel.send (messageToSend);
+							}
+							logUsedCommand ('-experimental loop ' + '[repeats: ' + (amountToLoop) + ']' + ' [message: ' + (messageToSend) + ']');
+							break;
+						}
 					}
 					break;
-				case 'help':
+				case 'help': case 'Help':
 					const helpEmbed = new MessageEmbed ()
 						.setAuthor ('IoxBot', 'https://cdn.discordapp.com/attachments/618926084750180363/742202185454190692/ioxbot_profile_photo.png')
 						.setColor (generateRandomShade('FF'))
@@ -167,37 +167,53 @@ client.on ('message', msg => {
 		}
 
 		//normal commands
-		case 'belt':
-		case 'Belt':
+		case lang.belt.trigger.one: case lang.belt.trigger.two:
 			// Create the belt attachment using MessageAttachment
 			const beltAttachment = new MessageAttachment ('https://cdn.discordapp.com/attachments/719955731821887602/733807048956837928/belt.jpg');
 			//send some text to the channel that the command was sent to
-			msg.channel.send ('ALRIGHT YOU AIN\'T LISTENIN\' SO YOU\'RE GETTING THE BELT!');
+			msg.channel.send (lang.belt.message);
 			//send the attachment made a couple lines ago
 			msg.channel.send (beltAttachment);
 			//logs the used command with the function created earlier
-			logUsedCommand ('belt');
+			logUsedCommand (lang.belt.log);
 			//makes sure that the next command isn't executed immediately after this one
 			break;
-		case 'uno_reverse_card':
-		case 'unoreversecard':
-			const unoAttachment = new MessageAttachment ('https://cdn.discordapp.com/attachments/719955731821887602/733807057811275797/uno_u.jpg');
-			msg.channel.send (unoAttachment);
-			logUsedCommand ('uno reverse card');
+		case lang.uno.trigger.one: case lang.uno.trigger.two: case lang.uno.trigger.separated.one:
+			//uses the "default" case to allow -uno reverse card (spaces)
+			switch (args[1]) {
+			default: case lang.uno.trigger.separated.two:
+				switch (args[2]) {
+				default: case lang.uno.trigger.separated.two:
+					const unoAttachment = new MessageAttachment ('https://cdn.discordapp.com/attachments/719955731821887602/733807057811275797/uno_u.jpg');
+					msg.channel.send (unoAttachment);
+					logUsedCommand (lang.uno.log);
+					break;
+				}
+			}
 			break;
-		case 'what\'s_a_tortoise':
-		case 'whatsatortoise':
-			msg.channel.send ('As we all know, an underdeveloped sad excuse for a turtle.');
-			logUsedCommand ('what\'s a tortoise');
+		case 'what\'s_a_tortoise': case 'whatsatortoise': case 'what\'s': case 'whats':
+			switch (args[1]) {
+			default: case 'a':
+				switch (args[2]) {
+				default: case 'tortoise':
+					msg.channel.send ('As we all know, an underdeveloped sad excuse for a turtle.');
+					logUsedCommand ('what\'s a tortoise');
+					break;
+				}
+			}
 			break;
-		case 'are_they_groovin?':
-		case 'aretheygroovin?':
-			const groovinAttachment = new MessageAttachment ('https://cdn.discordapp.com/attachments/719955731821887602/733807058654068756/groovin.jpg');
-			msg.channel.send (groovinAttachment);
-			logUsedCommand ('are they groovin\'?');
-			break;
-		case 'BotInfo':
-		case 'botinfo':
+		case 'are_they_groovin?': case 'aretheygroovin?': case 'are':
+			switch (args[1]) {
+			default: case 'they':
+				switch (args[2]) {
+				default: case 'groovin\'': case 'groovin': case 'groovin\'?': case 'groovin?':
+					const groovinAttachment = new MessageAttachment ('https://cdn.discordapp.com/attachments/719955731821887602/733807058654068756/groovin.jpg');
+					msg.channel.send (groovinAttachment);
+					logUsedCommand ('are they groovin\'?');
+					break;
+				}
+			}
+		case 'BotInfo': case 'botinfo':
 			const infoEmbed = new MessageEmbed ()
 				.setAuthor ('IoxBot', 'https://cdn.discordapp.com/attachments/618926084750180363/742202185454190692/ioxbot_profile_photo.png')
 				.setTitle ('General Information')
@@ -218,8 +234,7 @@ client.on ('message', msg => {
 			msg.channel.send (veryfineattachment);
 			logUsedCommand ('die');
 			break;
-		case 'coinflip':
-		case 'Coinflip':
+		case 'coinflip': case 'Coinflip':
 			//generates a random number between 1 and 2 and stores it in "random"
 			var random = (Math.ceil (Math.random () * 2));
 			//declares a variable, "messageToSend", which will be used to store the message text
@@ -227,22 +242,22 @@ client.on ('message', msg => {
 			//declares a variable, "coinFace", which will be used to store an image link for the corresponding side of the coin
 			var coinFace;
 			switch (random) {
-			//checks if the random number is one
-			case '1':
-				//changes messageToSend to "Your coin landed on heads!"
-				messageToSend = 'Your coin landed on heads!';
-				//sets coinFace to a picture of a coin on heads
-				coinFace = 'https://cdn.discordapp.com/attachments/728781398911221795/739249818081296384/coin_heads.jpg';
-				//break because switches are a bit jank in that they'll execute the next case if you don't break or return
-				break;
-			case '2':
-				//changes messageToSend to "Your coin landed on tails!"
-				messageToSend = 'Your coin landed on tails!';
-				//sets coinFace to a picture of a coin on tails
-				coinFace = 'https://cdn.discordapp.com/attachments/728781398911221795/739249795469803612/coin_tails.jpg';
-				break;
-			default:
-				console.log ('invalid number; command failed to execute');
+				//checks if the random number is one
+				case '1':
+					//changes messageToSend to "Your coin landed on heads!"
+					messageToSend = 'Your coin landed on heads!';
+					//sets coinFace to a picture of a coin on heads
+					coinFace = 'https://cdn.discordapp.com/attachments/728781398911221795/739249818081296384/coin_heads.jpg';
+					//break because switches are a bit jank in that they'll execute the next case if you don't break or return
+					break;
+				case '2':
+					//changes messageToSend to "Your coin landed on tails!"
+					messageToSend = 'Your coin landed on tails!';
+					//sets coinFace to a picture of a coin on tails
+					coinFace = 'https://cdn.discordapp.com/attachments/728781398911221795/739249795469803612/coin_tails.jpg';
+					break;
+				default:
+					console.log ('invalid number; command failed to execute');
 			}
 			//sends "messageToSend" in an embed with some added things
 			const coinEmbed = new MessageEmbed ()
@@ -258,23 +273,25 @@ client.on ('message', msg => {
 		case 'info':
 			//creates a second switch for arguments after the original "-info" command
 			switch (args[1]) {
-				case 'creation_date', 'creationdate':
-					//creates an embed, which is then edited by the .set code
-					const dateEmbed = new MessageEmbed ()
-						//adds a field at the top of the embed which shows "IoxBot", with a small image dictated by the link
-						.setAuthor ('IoxBot', 'https://cdn.discordapp.com/attachments/618926084750180363/742202185454190692/ioxbot_profile_photo.png')
-						//set the title of the embed
-						.setTitle ('Created on June 17th, 2020')
-						// Set the color of the embed's side thingy, in this case green
-						.setColor (generateRandomShade ('FF'))
-						// Set the main content of the embed
-						.setDescription ('Last update was ' + (config.lastUpdate));
-						// Send the embed to the same channel as the command
-					msg.channel.send (dateEmbed);
-					logUsedCommand ('info : creation date');
-					break;
-				case 'version':
-				case 'Version':
+				case 'creation_date': case 'creationdate': case 'creation':
+					switch (args[2]) {
+					default: case 'date':
+						//creates an embed, which is then edited by the .set code
+						const dateEmbed = new MessageEmbed ()
+							//adds a field at the top of the embed which shows "IoxBot", with a small image dictated by the link
+							.setAuthor ('IoxBot', 'https://cdn.discordapp.com/attachments/618926084750180363/742202185454190692/ioxbot_profile_photo.png')
+							//set the title of the embed
+							.setTitle ('Created on June 17th, 2020')
+							// Set the color of the embed, in this case green
+							.setColor (generateRandomShade ('FF'))
+							// Set the main content of the embed
+							.setDescription ('Last update was ' + (config.lastUpdate));
+							// Send the embed to the same channel as the command
+						msg.channel.send (dateEmbed);
+						logUsedCommand ('info : creation date');
+						break;
+					}
+				case 'version': case 'Version':
 					const versionEmbed = new MessageEmbed ()
 						.setAuthor ('IoxBot', 'https://cdn.discordapp.com/attachments/618926084750180363/742202185454190692/ioxbot_profile_photo.png')
 						.setTitle (config.version)
@@ -282,8 +299,7 @@ client.on ('message', msg => {
 					msg.channel.send (versionEmbed);
 					logUsedCommand ('info : version');
 					break;
-				case 'author':
-				case 'Author':
+				case 'author': case 'Author':
 					const authorEmbed = new MessageEmbed ()
 						.setAuthor ('IoxBot', 'https://cdn.discordapp.com/attachments/618926084750180363/742202185454190692/ioxbot_profile_photo.png')
 						.setTitle ('IoxBot has been programmed by Ioxom')
@@ -293,28 +309,25 @@ client.on ('message', msg => {
 					msg.channel.send (authorEmbed);
 					logUsedCommand ('info : author');
 					break;
-				case 'commands':
-				case 'Commands':
+				case 'commands': case 'Commands':
 					const commandsEmbed = new MessageEmbed ()
 						.setAuthor ('IoxBot', 'https://cdn.discordapp.com/attachments/618926084750180363/742202185454190692/ioxbot_profile_photo.png')
 						.setTitle ('IoxBot\'s commands are :')
 						.setColor (generateRandomShade ('FF'))
 						//adds fields, which are different sections of the embed
 						.addFields (
-							{ name: 'Normal Commands', value: (config.commands), inline: false },
-							{ name: 'Prefixless Commands', value: (config.prefixlessCommands), inline: false }
+							{ name: 'Normal Commands', value: (updates.commands), inline: false },
+							{ name: 'Prefixless Commands', value: (updates.prefixlessCommands), inline: false }
 						);
 					msg.channel.send (commandsEmbed);
 					logUsedCommand ('info : commands');
 					break;
-				case 'GitHub':
-				case 'Github':
-				case 'github':
+				case 'GitHub': case 'Github': case 'github': case 'gitHub':
 					const githubEmbed = new MessageEmbed ()
 						.setAuthor ('IoxBot', 'https://cdn.discordapp.com/attachments/618926084750180363/742202185454190692/ioxbot_profile_photo.png')
 						.setTitle ('Source code For IoxBot can be found here')
     					.setColor (generateRandomShade ('FF'))
-    					.setURL (config.githubURL);
+    					.setURL ('https://github.com/Ioxom/IoxBot');
 					msg.channel.send (githubEmbed);
 					logUsedCommand ('info : github');
 					break;
