4caabcc427fdb8eb2036aa30893b8157022e1a32
Ioxom <jackwheppner@gmail.com>
2020-12-07 10:42:14 -0600

    remove token visibility

--- src/main/java/ca/ioxom/ioxbot/Main.java
+++ src/main/java/ca/ioxom/ioxbot/Main.java
@@ -4,16 +4,23 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
 import javax.swing.JFrame;
-import java.awt.*;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Main {
-    public static final String TOKEN = "NzIyODM1MjkwNjQ0ODA3NzEx.Xuo20g.gPUYmyX7V8tEFCwPvhhpzcjb0U4";
+    public static String TOKEN;
+    public static final String VERSION = "0.1.0";
     public static void main(String[] arguments) throws Exception {
-        JDA api = JDABuilder.createDefault(TOKEN).build();
-        api.addEventListener(new Commands.Listener());
-        JFrame frame = new JFrame("ioxbot");
+        try (Scanner scanner = new Scanner(Paths.get("token.txt"))) {
+            TOKEN = scanner.nextLine();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        JFrame frame = new JFrame("ioxbot v " + VERSION);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(250, 100);
         frame.setVisible(true);
+        JDA api = JDABuilder.createDefault(TOKEN).build();
+        api.addEventListener(new Commands.Listener());
     }
 }
