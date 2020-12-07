fffe331ccc97807f5d5ae59dc9d3d8d395c06019
Ioxom <jackwheppner@gmail.com>
2020-12-07 15:00:56 -0600

    implement error if no token is found

--- src/main/java/ca/ioxom/ioxbot/Main.java
+++ src/main/java/ca/ioxom/ioxbot/Main.java
@@ -12,13 +12,19 @@ public class Main {
     public static final String PREFIX = "-";
     public static IoxbotFrame frame;
     public static void main(String[] arguments) throws Exception {
+        frame = new IoxbotFrame();
+        frame.init();
         try (Scanner scanner = new Scanner(Paths.get("token.txt"))) {
             TOKEN = scanner.nextLine();
         } catch (Exception e) {
-            System.out.println(e);
+            frame.log("no token found; closing ioxbot");
+            try {
+                Thread.sleep(5000);
+                System.exit(2);
+            } catch (Exception f) {
+                frame.log(f.toString());
+            }
         }
-        frame = new IoxbotFrame();
-        frame.init();
         JDA api = JDABuilder.createDefault(TOKEN).build();
         api.addEventListener(new Commands.Listener());
         frame.log("initialized jda; ioxbot is ready to go");
