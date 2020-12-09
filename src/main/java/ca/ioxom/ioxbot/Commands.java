cd8edb04a3d9e32348094363e9c52411438377c5
Ioxom <jackwheppner@gmail.com>
2020-12-08 19:04:13 -0600

    fix ping command, add -gh

--- src/main/java/ca/ioxom/ioxbot/Commands.java
+++ src/main/java/ca/ioxom/ioxbot/Commands.java
@@ -8,23 +8,33 @@ import org.jetbrains.annotations.NotNull;
 
 public class Commands {
     public static class Listener extends ListenerAdapter {
+        long start, end;
+        boolean checkingPing;
         @Override
         public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+            if (checkingPing) {
+                event.getChannel().editMessageById(event.getChannel().getLatestMessageId(),"ioxbot's ping is: " + 10. * (end - start) / 1000000 + "ms" ).queue();
+                checkingPing = false;
+            }
             ExtraCommands.noYoutube(event);
 
             if (!event.getMessage().getContentRaw().startsWith("-")) return;
             String messageContent = event.getMessage().getContentRaw().split("-", 2)[1].toLowerCase();
-            switch (messageContent) {
+            switch (messageContent.split(" ")[0]) {
                 case "ping":
-                    long start = System.nanoTime();
+                    checkingPing = true;
+                    start = System.nanoTime();
                     event.getChannel().sendMessage("calculating ping...").queue();
-                    long end = System.nanoTime();
-                    event.getChannel().sendMessage("ioxbot's ping is: " + 10. * (end - start) / 1000000 + "ms").queue();
+                    end = System.nanoTime();
                     Main.frame.logCommand("check ping", event);
                     break;
                 case "help":
                     event.getChannel().sendMessage("help is under construction").queue();
                     Main.frame.logCommand("help", event);
+                    break;
+                case "gh":
+                    event.getChannel().sendMessage("https://github.com/" + messageContent.split(" ")[1]).queue();
+                    Main.frame.logCommand("gh", event);
                     break;
             }
         }
--- src/main/java/ca/ioxom/ioxbot/stuff/ExtraCommands.java
+++ src/main/java/ca/ioxom/ioxbot/stuff/ExtraCommands.java
@@ -5,7 +5,7 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class ExtraCommands {
     public static void noYoutube(MessageReceivedEvent event) {
-        if (event.getAuthor().getId().equals("382659726778957858") && event.getMessage().getContentRaw().startsWith("https://www.youtu")) {
+        if (event.getAuthor().getId().equals("382659726778957858") && event.getMessage().getContentRaw().contains("https://www.youtu") || event.getMessage().getContentRaw().contains("www.youtube")) {
             event.getMessage().delete().queue();
             event.getChannel().sendMessage("bad").queue();
             Main.frame.log("forg used very bad");
