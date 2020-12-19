9d104a8fe30c5ae7661f00f6785fa49ff33b87a5
Ioxom <jackwheppner@gmail.com>
2020-12-18 20:57:56 -0600

    rotating status + use ScheduledExecutorService to run through prefix and help

--- src/main/java/ca/ioxom/ioxbot/other/StatusSetter.java
+++ src/main/java/ca/ioxom/ioxbot/other/StatusSetter.java
@@ -5,10 +5,22 @@ import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.managers.Presence;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
 public class StatusSetter extends ListenerAdapter {
+    private final ScheduledExecutorService scheduler =
+            Executors.newScheduledThreadPool(1);
+
     @Override
     public void onReady(ReadyEvent event) {
         Presence presence = event.getJDA().getPresence();
-        presence.setActivity(Activity.playing(Config.status));
+
+        Runnable setStatusPrefix = () -> presence.setActivity(Activity.playing("prefix | " + Config.prefix));
+        Runnable setStatusHelp = () -> presence.setActivity(Activity.playing("help | " + Config.prefix + "help"));
+        scheduler.scheduleAtFixedRate(setStatusPrefix, 0, 20, SECONDS);
+        scheduler.scheduleAtFixedRate(setStatusHelp, 20, 20, SECONDS);
     }
 }
