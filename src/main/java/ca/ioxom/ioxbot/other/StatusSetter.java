01df92f2d5490764d3a7df5f01b2737addda230c
Ioxom <jackwheppner@gmail.com>
2020-12-18 21:23:14 -0600

    make rotating status actually work

--- src/main/java/ca/ioxom/ioxbot/other/StatusSetter.java
+++ src/main/java/ca/ioxom/ioxbot/other/StatusSetter.java
@@ -11,16 +11,31 @@ import java.util.concurrent.ScheduledExecutorService;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class StatusSetter extends ListenerAdapter {
-    private final ScheduledExecutorService scheduler =
-            Executors.newScheduledThreadPool(1);
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
     @Override
     public void onReady(ReadyEvent event) {
-        Presence presence = event.getJDA().getPresence();
+        StatusRunnable setStatus = new StatusRunnable(event.getJDA().getPresence());
+        scheduler.scheduleAtFixedRate(setStatus, 0, 20, SECONDS);
+    }
 
-        Runnable setStatusPrefix = () -> presence.setActivity(Activity.playing("prefix | " + Config.prefix));
-        Runnable setStatusHelp = () -> presence.setActivity(Activity.playing("help | " + Config.prefix + "help"));
-        scheduler.scheduleAtFixedRate(setStatusPrefix, 0, 20, SECONDS);
-        scheduler.scheduleAtFixedRate(setStatusHelp, 20, 20, SECONDS);
+    public static class StatusRunnable implements Runnable {
+        private final String[] statuses = {"prefix | " + Config.prefix, "help | " + Config.prefix + "help"};
+        public int i;
+        public Presence presence;
+        public StatusRunnable(Presence presence) {
+            this.presence = presence;
+            this.i = 0;
+        }
+
+        @Override
+        public void run() {
+            if (i == 0) {
+                i = 1;
+            } else {
+                i = 0;
+            }
+            this.presence.setActivity(Activity.playing(this.statuses[i]));
+        }
     }
 }
