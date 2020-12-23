a2e4cb68ecf7d1c85837976a22d7ca307c623985
Ioxom <jackwheppner@gmail.com>
2020-12-23 16:39:06 -0600

    bump version

--- src/main/java/ca/ioxom/ioxbot/frame/Main.java
+++ src/main/java/ca/ioxom/ioxbot/frame/Main.java
@@ -11,7 +11,7 @@ import java.security.SecureRandom;
 
 public class Main {
     public static final SecureRandom random = new SecureRandom();
-    public static final String VERSION = "0.1.2";
+    public static final String VERSION = "0.1.3";
     public static IoxbotFrame frame;
     public static void main(String[] args) {
         //create frame
