e2b6c09f9d041dc470fd66d806a93939a168feb0
Ioxom <jackwheppner@gmail.com>
2021-01-25 15:50:55 -0600

    make version in build.gradle universal version

--- src/main/java/ca/ioxom/ioxbot/Main.java
+++ src/main/java/ca/ioxom/ioxbot/Main.java
@@ -1,8 +1,8 @@
 package ca.ioxom.ioxbot;
 
+import ca.ioxom.ioxbot.config.ConfigObject;
 import ca.ioxom.ioxbot.frame.IoxbotFrame;
 import ca.ioxom.ioxbot.listeners.MainListener;
-import ca.ioxom.ioxbot.config.ConfigObject;
 import ca.ioxom.ioxbot.listeners.StatusSetter;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
@@ -10,21 +10,47 @@ import net.dv8tion.jda.api.JDABuilder;
 import javax.security.auth.login.LoginException;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Properties;
 import java.util.Random;
 
 public class Main {
     public static ConfigObject config = new ConfigObject();
     public static final Random random = new Random();
-    public static final String VERSION = "0.2.0";
+    public static String VERSION;
     public static IoxbotFrame frame;
     public static void main(String[] args) {
+        //get version
+        try {
+            Properties properties = new Properties();
+            String fileName = "ioxbot.properties";
+            InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("ioxbot.properties");
+
+            //load input stream
+            if (inputStream != null) {
+                properties.load(inputStream);
+            } else {
+                throw new FileNotFoundException("property file '" + fileName + "' not found in the classpath");
+            }
+
+            VERSION = properties.getProperty("version");
+            properties.clear();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         //create frame
         frame = new IoxbotFrame();
         frame.init();
         //configure
         config.configure();
+        //throw error if version is not found
+        if (VERSION == null) {
+            VERSION = "0.0.0";
+            frame.throwError("could not get version; defaulting to 0.0.0");
+        }
         //log in
         JDA api = null;
         try {
